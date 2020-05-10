1eb670e235321f8a25d6e9da9635013e13c84d8e
Samir Azzouz <s.azzouz97@gmail.com>
2020-05-10 18:55:23 +0100

    the final project wasn't actually the latest, reverted back to earlier commits

--- crm/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-#from .models import *
-from . models import Customer, Order, Product
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
