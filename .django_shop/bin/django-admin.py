c90563dd2884c88a51b1b8a89e969ed2c4b8d881
Samir Azzouz <s.azzouz97@gmail.com>
2020-05-10 16:20:51 +0100

    Init with stripe notebook

---
+++ .django_shop/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/szzz/Documents/dev/github/django-shop/django-shop/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
